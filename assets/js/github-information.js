1c29528b67b05a88368d5279a7dab42905268c89
marinamarshall <marinamarshall2@gmail.com>
2022-02-07 20:07:27 +0000

    Add userInformationHTML function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -7,8 +7,11 @@ function userInformationHTML(user) {
     </h2>
     <div class="gh-content">
         <div class="gh-avatar">
-            <div></div>
+            <a href="${user.html_url}" target="_blank">
+                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}"/>
+            </a>
         </div>
+        <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
     </div>`    
 }
 
